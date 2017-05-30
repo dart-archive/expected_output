59c8b388bcca03f7fbbe6cdcd0eb25e1662f453d
Sam Rawlins <sam.rawlins@gmail.com>
2017-05-30 15:56:25 -0700

    Implement new features: (#5)
    * `dataCases` and `dataCasesUnder` now accept `extension:` and
      `recursive:` named arguments.
    * `dataCasesUnder` now accepts a `subdirectory:` named argument.
    * The data case parser now marks cases that start with "skip:" as
      skipped.

---
+++ test/library_test.dart
@@ -0,0 +1,36 @@
+library expected_output.test.recursive_test;
+
+import 'package:expected_output/expected_output.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('finds test cases when passed a library Symbol', () {
+    var iterator = dataCasesUnder(library: #expected_output.test.recursive_test,
+            subdirectory: 'recursive_data', recursive: false)
+        .iterator;
+    iterator.moveNext();
+    var dataCase = iterator.current;
+
+    expect(dataCase.directory, 'recursive_data');
+    expect(dataCase.file, 'foo');
+    expect(dataCase.description, 'line 2: data case in the primary directory');
+
+    var iteratorIsEmpty = !iterator.moveNext();
+    expect(iteratorIsEmpty, isTrue);
+  });
+
+  test('finds test cases when passed a library Symbol, and subdirectories', () {
+    var iterator = dataCasesUnder(library: #expected_output.test.recursive_test,
+            subdirectory: 'recursive_data/deep', recursive: false)
+        .iterator;
+    iterator.moveNext();
+    var dataCase = iterator.current;
+
+    expect(dataCase.directory, 'deep');
+    expect(dataCase.file, 'bar');
+    expect(dataCase.description, 'line 2: a deeper data case');
+
+    var iteratorIsEmpty = !iterator.moveNext();
+    expect(iteratorIsEmpty, isTrue);
+  });
+}
