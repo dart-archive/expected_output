6543dd3bce90c600a8671a5ebb8fa2ae65790e06
Sam Rawlins <sam.rawlins@gmail.com>
2017-06-05 18:58:32 -0700

    Add an API for individual files (#6)
    Add an API for individual files

---
+++ test/individual_file_test.dart
@@ -0,0 +1,57 @@
+library expected_output.test.individual_file_test;
+
+import 'dart:mirrors';
+
+import 'package:expected_output/expected_output.dart';
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
+
+void main() {
+  // Locate the "test" directory. Use mirrors so that this works with the test
+  // package, which loads this suite into an isolate.
+  var testDir = p.dirname(currentMirrorSystem()
+      .findLibrary(#expected_output.test.individual_file_test)
+      .uri
+      .toFilePath());
+  testDir = p.join(testDir, 'individual_files');
+
+  test('looks for data cases in individual files', () {
+    var iterator = dataCasesInFile(path: p.join(testDir, 'a.unit')).iterator;
+    iterator.moveNext();
+    var dataCase = iterator.current;
+
+    expect(dataCase.directory, endsWith('individual_files'));
+    expect(dataCase.file, 'a');
+    expect(dataCase.description, 'line 2: description 1');
+
+    iterator.moveNext();
+    dataCase = iterator.current;
+
+    expect(dataCase.directory, endsWith('individual_files'));
+    expect(dataCase.file, 'a');
+    expect(dataCase.description, 'line 6: description 2');
+
+    var iteratorIsEmpty = !iterator.moveNext();
+    expect(iteratorIsEmpty, isTrue);
+  });
+
+  test('looks for data cases in individual files', () {
+    var iterator = dataCasesInFile(path: p.join(testDir, 'b.unit')).iterator;
+    iterator.moveNext();
+    var dataCase = iterator.current;
+
+    expect(dataCase.directory, endsWith('individual_files'));
+    expect(dataCase.file, 'b');
+    expect(dataCase.description, 'line 2: description 3');
+
+    iterator.moveNext();
+    dataCase = iterator.current;
+
+    expect(dataCase.directory, endsWith('individual_files'));
+    expect(dataCase.file, 'b');
+    expect(dataCase.description, 'line 6: description 4');
+
+    var iteratorIsEmpty = !iterator.moveNext();
+    expect(iteratorIsEmpty, isTrue);
+  });
+}
